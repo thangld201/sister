71e6701f48057b11dfa77d4653af01cca6dd7a35
ThangLD201 <thangld.bkhn@gmail.com>
2022-06-12 10:41:05 +0700

    Update word_embedders.py

--- sister/word_embedders.py
+++ sister/word_embedders.py
@@ -14,7 +14,7 @@ def get_fasttext(lang: str = "en"):
         "en": "https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.simple.zip",
         "ja": "https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.ja.zip",
         "fr": "https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.fr.zip",
-        "vi": "https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.vi.300.bin.gz"
+        "vi": "https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.vi.zip"
     }
     path = download.cached_download(urls[lang])
     path = Path(path)
